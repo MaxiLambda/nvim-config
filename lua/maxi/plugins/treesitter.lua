e94423af43a92df47374be9a0e9dce62badf10d4
Maxi <maximilian.lincks@gmx.net>
2024-02-08 20:06:47 +0100

    added gitsigns

---
+++ lua/maxi/plugins/treesitter.lua
@@ -0,0 +1,42 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
+    config = function()
+      -- import nvim-treesitter plugin
+      local treesitter = require("nvim-treesitter.configs")
+
+      -- configure treesitter
+      treesitter.setup({ -- enable syntax highlighting
+        highlight = {
+          enable = true,
+        },
+        -- enable indentation
+        indent = { enable = true },
+        -- enable autotagging (w/ nvim-ts-autotag plugin)
+        -- ensure these language parsers are installed
+        ensure_installed = {
+          "json",
+          "yaml",
+          "markdown",
+          "markdown_inline",
+          "bash",
+          "lua",
+          "vim",
+          "gitignore",
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<C-space>",
+            node_incremental = "<C-space>",
+            scope_incremental = false,
+            node_decremental = "<bs>",
+          },
+        },
+      })
+
+    end,
+  },
+}
